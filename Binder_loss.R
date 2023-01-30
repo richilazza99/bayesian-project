99cd9c327ab381b12a318b6cad933d379c198952
AnnaLunghi <120175633+AnnaLunghi@users.noreply.github.com>
2023-01-30 12:16:15 +0100

    Ho aggiunto su simplified model la Binder loss su R

--- Binder_loss.R
+++ Binder_loss.R
@@ -2,13 +2,14 @@ if(!require('salso')) {
   install.packages('salso')
   library('salso')
 }
+s <- read.delim(choose.files(), header=F,',')
 sm=as.matrix(s)
-est <- salso(sm,"binder",maxNClusters = 7, nCores=1)
+est <- salso(sm,"binder",maxNClusters = 10, nCores=1)
 summ <- summary(est)
 plot(summ, type="heatmap")
-plot(summ, type="mds")
-plot(summ, type="dendrogram")
+#plot(summ, type="mds")
+#plot(summ, type="dendrogram")
 
 s_best=summ[["estimate"]]
-s_squared <- matrix(s_best, ncol=7, byrow=T)
-s_squared
+s_out <- matrix(s_best, ncol=1, byrow=T)
+write.csv(s_out, "s_binder.csv")
