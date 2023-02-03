a2cf7e9934efafd3ff8090f3aa444d86147df46d
fedebuse <fedepa.9961@hotmail.it>
2023-02-03 10:03:25 +0100

    script per creare mappa cluster, dato la matrice di cluster dei beta

---
+++ mappa province cluster.R
@@ -0,0 +1,37 @@
+library(maptools)
+library(ggplot2)
+library(tidyverse)
+
+#cluster.beta=read.csv("#######")  mettere matrice cluster allocation dei beta
+#dataset=read.csv("Fecondita.csv")   carica il dataset completo
+
+
+cluster.beta$region=sort(unique(dataset$Territorio))
+colnames(cluster.beta)[2]="cluster"
+cluster.beta$cluster=factor(cluster.beta$cluster)
+
+
+#setdiff(cluster.beta$region,unique(italy_map$region))
+#setdiff(unique(italy_map$region),cluster.beta$region)
+
+cluster.beta$region[75]="Trento"
+cluster.beta$region[74]="Bolzano-Bozen"
+cluster.beta$region[98]="Aosta"
+cluster.beta$region[79]="Reggio Emilia"
+cluster.beta$region[34]="Forli'"
+cluster.beta$region[78]="Reggio Calabria"
+
+cluster.beta=cluster.beta[-c(9,13,27,30,45,47,56,73,81,101,104),c(2,3)]
+
+italy_map <- map_data("italy")
+italy_proj <- "+proj=aea +lat_1=38.15040684902542
++lat_2=44.925490198742295 +lon_0=12.7880859375"
+
+mapdata=left_join(italy_map,cluster.beta,by='region')
+
+
+map.cluster = ggplot(mapdata,aes(x=long, y= lat, group=group)) +
+  geom_polygon(aes(fill=cluster),color='black')
+map.cluster
+
+
